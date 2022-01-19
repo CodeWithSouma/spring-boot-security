baf404c6cfc628f86842208b2c38f1ecd98c6c46
CodeWithSouma <souma.hitech@gmail.com>
2022-01-19 21:11:07 +0530

    create our own user and configure bcrypt password encoder

--- src/main/java/com/codewithsouma/springbootsecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/com/codewithsouma/springbootsecurity/security/ApplicationSecurityConfig.java
@@ -1,13 +1,23 @@
 package com.codewithsouma.springbootsecurity.security;
 
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
+@AllArgsConstructor
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+    private final PasswordEncoder passwordEncoder;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -18,5 +28,16 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails soumadipDeyUser = User.builder()
+                .username("Soumadip Dey")
+                .password(passwordEncoder.encode("Soumadip@1"))
+                .roles("STUDENT") // ROLE_STUDENT
+                .build();
+        return new InMemoryUserDetailsManager(soumadipDeyUser);
     }
 }
