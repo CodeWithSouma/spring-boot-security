53c2fb25f255f731db9bc14192c6903db8692b29
CodeWithSouma <souma.hitech@gmail.com>
2022-01-20 20:05:51 +0530

    Enable global method security

--- src/main/java/com/codewithsouma/springbootsecurity/student/StudentManagementController.java
+++ src/main/java/com/codewithsouma/springbootsecurity/student/StudentManagementController.java
@@ -1,5 +1,6 @@
 package com.codewithsouma.springbootsecurity.student;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,22 +14,27 @@ public class StudentManagementController {
             new Student(3,"Arijit")
     );
 
+    //    hasRole('ROLE_') hasAnyRole('ROLE_') hasAuthority('permission') hasAnyAuthority('permission')
     @GetMapping
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_ADMINTRAINEE')")
     public List<Student> getAllStudents(){
         return STUDENTS;
     }
 
     @PostMapping
+    @PreAuthorize("hasAnyAuthority('student:write')")
     public void registerNewStudent(@RequestBody Student student){
         System.out.println(student);
     }
 
     @DeleteMapping(path = "/{studentId}")
+    @PreAuthorize("hasAnyAuthority('student:write')")
     public void deleteStudent(@PathVariable("studentId") int studentId){
         System.out.println(studentId);
     }
 
     @PutMapping(path = "/{studentId}")
+    @PreAuthorize("hasAnyAuthority('student:write')")
     public void updateStudent( @PathVariable("studentId") int studentId, @RequestBody Student student){
         System.out.println(String.format("%s %s",studentId,student));
     }
